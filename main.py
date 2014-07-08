ca55af7ec68d2a888fd2d601228c34c7a89fdcc6
Tim Harshman <goteamtim+git@gmail.com>
2014-07-07 21:03:37 -0700

    Updates
    take 4 pictures and add option to create montage

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import picamera
 import pygame
 import sys
 import os
+from PIL import Image, ImageFilter
 
 capturedImageFileNames = []
 
@@ -12,30 +13,56 @@ day = time.strftime("%d%m%y")
 x = int(raw_input("Enter 1 if you love America! "))
 
 if x==1:
-
+        
+        imageFile = '/home/pi/photobooth/%s/image%s.jpeg'% (day, now)
         # INIT CAMERA
         camera = picamera.PiCamera()
-        
         camera.vflip = True
         camera.hflip = False
         camera.brightness = 60
         camera.resolution = (612,612)
-        #camera.start_preview()
-        
-        #time.sleep(0.5)
-        #camera.capture('image.jpeg', format='jpeg')
-        #camera.stop_preview()
 
         if not os.path.exists("/home/pi/photobooth/"+day): #If the folder for today does not exist create it
                 os.makedirs("/home/pi/photobooth/"+day)
         
-        camera.capture('/home/pi/photobooth/%s/image%s.jpeg'% (day, now)) #save in the today folder with current time
+        for x in range (0,4):
+            camera.capture(imageFile) #save in the today folder with current time
+            time.sleep(1)
+            capturedImageFileNames[x] = imageFile
         camera.close()
-        print 'Your photo has been taken.'
-        sys.exit(0)
+        print 'Your photos have been taken.'
+        sys.exit()
+        
 
 else :
 	print "Thank you, no photos taken."
-	exit()
+	sys.exit()
 
-sys.exit(0)
+j = str(raw_input('Would you like to create a montage? (yes or no)'))
+
+if j=='no':
+    sys.exit()
+elif j=='yes':
+    out = Image.new("RGB", (1269, 1269), "black") #actual final image
+    '''
+    Image will be as follows
+    [1][2]
+    [3][4]
+    '''
+    print "Building..."
+    out.paste(Image.open(capturedImageFileNames[0]), (int(15), int(15)))# first is x then y
+    out.paste(Image.open(capturedImageFileNames[1]), (int(642), int(15)))#image number two
+    out.paste(Image.open(capturedImageFileNames[2]), (int(15), int(642))) #image number three
+    out.paste(Image.open(capturedImageFileNames[3]), (int(642), int(642))) #image number four
+
+    print "Saving..."
+    if not os.path.exists("/home/pi/photobooth/"+day): #If the folder for today does not exist create it
+        os.makedirs("/home/pi/photobooth/"+day)
+    out.save('/home/pi/photobooth/%s/montage%s.jpeg'% (day, now)) #save in the today folder with current time
+
+    print "Done."
+
+    sys.exit(0)
+
+else: 
+    sys.exit(0)
