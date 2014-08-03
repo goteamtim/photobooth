1f5848604be0cd62e0853876f46a9b72d4d70996
Tim Harshman <goteamtim+git@gmail.com>
2014-08-03 08:08:25 -0700

    Added picture count
    Added a .dat file to hold the total number of pictures taken with the
    photobooth.  Also added the beginning of the google drive upload
    integration.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import httplib2
+import pprint
 import time
 import picamera
 #import pygame
@@ -9,6 +11,27 @@ from PIL import Image, ImageFilter
 import array
 import string
 import RPi.GPIO as GPIO
+
+from apiclient.discovery import build
+from apiclient.http import MediaFileUpload
+from oauth2client.client import OAuth2WebServerFlow
+
+#SETUP VARIABLES
+CLIENT_ID = 'YOUR_CLIENT_ID'
+CLIENT_SECRET = 'YOUR_CLIENT_SECRET'
+
+# Check https://developers.google.com/drive/scopes for all available scopes
+OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
+
+# Redirect URI for installed apps
+REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
+
+#Include error handling later
+#if not os.path.isfile("/home/totalPictures.dat"): 
+        #DataIn = open("/home/totalPictures.dat","w")#Create a writeable file if it doesnt exist
+dataIn = open('/home/totalPictures.dat',"r")
+totalPicturesTaken = int(dataIn.readline())
+dataIn.close()
 
 #SETUP GPIO
 GPIO.setmode(GPIO.BCM)
@@ -37,7 +60,7 @@ def takePicture(input):
         camera.capture(temp) #save in the today folder with current time
         capturedImageFileNames[x] = temp
     camera.close()
-    print 'Your photos have been taken.'
+    print 'Your photos have been taken, creating montage...'
     out = Image.new("RGB", (1269, 1269), "black") #actual final image
     '''
     Final image will be as follows
@@ -50,18 +73,30 @@ def takePicture(input):
     out.paste(Image.open(capturedImageFileNames[2]), (int(15), int(642))) #image number three
     out.paste(Image.open(capturedImageFileNames[3]), (int(642), int(642))) #image number four
     print "Saving..."
+	totalPicturesTaken+=1
     if not os.path.exists("/home/pi/photoboothPhotos/"+day): #If the folder for today does not exist create it
         os.makedirs("/home/pi/photoboothPhotos/"+day)
     out.save('/home/pi/photoboothPhotos/%s/montage%s.jpeg'% (day, now)) #save in the today folder with current time
-	#Bug,
-    print "Done."
+    print "Picture saved locally, uploading..."
+	#writing total pictures amount to local file
+	try:
+		#uploading to drive
+		#Calling out to server and setting total pictures
+		break
+	except :
+		print "Error uploading, file still saved locally."
+		
+	
 
 GPIO.add_event_detect(23, GPIO.FALLING, callback=takePicture, bouncetime=200) #Button pres calls the take picture function
 
 
 while True:
-    print "Press button one to take a picture and 2 to exit"
+    print "Press button one to take a picture and 2 to exit."
     GPIO.wait_for_edge(24, GPIO.FALLING)
     GPIO.Cleanup()
     print "Exiting..."
+	f = open('/home/totalPictures.dat',"w")
+	f.write(totalPicturesTaken)
+	f.close()
     Sys.exit(0)
